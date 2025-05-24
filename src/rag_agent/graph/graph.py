7a5bdabee01e1ffc7e3a06cc942cdb87b1e27d33
Matthias <126894819+DRXD1000@users.noreply.github.com>
2025-05-24 21:59:23 +0200

    Precommit AutoUpdate

--- src/rag_agent/graph/graph.py
+++ src/rag_agent/graph/graph.py
@@ -68,7 +68,4 @@ def reranking(state: AgentState, limit: int = 4) -> AgentState:
 
     best_indices = np.argsort(scores)[-limit:][::-1]
 
-
-
-
     return {"documents": [state["documents"][idx] for idx in best_indices]}
