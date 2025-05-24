0d597900064f1cfca6d40c1e94fdfd598d70b5a9
Matthias <126894819+DRXD1000@users.noreply.github.com>
2025-05-24 21:22:38 +0200

    Setting Default for Embedding Model

--- src/rag_agent/llms/models.py
+++ src/rag_agent/llms/models.py
@@ -12,7 +12,7 @@ def initialize_ollama_llm(model_name: str, temperature: float, **kwargs: Any) ->
     return ChatOllama(model=model_name, temperature=temperature, **kwargs)
 
 
-def initialize_embedding_model(model_name: str) -> OllamaEmbeddings:
+def initialize_embedding_model(model_name: str = "bge-m3:567m") -> OllamaEmbeddings:
     """Initialize Embedding Model."""
     return OllamaEmbeddings(model=model_name)
 
